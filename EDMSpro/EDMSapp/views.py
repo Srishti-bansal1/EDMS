5dcac4507207ad911fa76336244e26ab0f1433eb
srishti <srishtibansal241@gmail.com>
2024-01-25 10:55:35 +0530

    CRUD api in views complete

--- EDMSpro/EDMSapp/views.py
+++ EDMSpro/EDMSapp/views.py
@@ -2,60 +2,79 @@ from django.shortcuts import render
 
 # Create your views here.
 from EDMSapp.models import EDMSmodel
-from rest_framework import viewsets
-
 from EDMSapp.serializers import EDMSSerializer
 from rest_framework import status
 from rest_framework.response import Response
-
 from rest_framework.decorators import api_view
 
-class EDMSViewSet (viewsets.ModelViewSet):
-    emp = EDMSmodel.objects.all()
-    seralizer_class = EDMSSerializer
-    
-@api_view(['POST'])
-def create(self, request, *args, **kwargs):
-    # retrieving the data of the request
-    serializer = self.get_serializer(data=request.data)
-    # raising exception if it invalid
-    serializer.is_valid(raise_exception=True)
-    # saving
-    serializer.save(serializer)
-    # returning data and status as a response
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
+    
 @api_view(['GET'])
     # list action
-def list(self, request):
-    serializer = self.get_serializer(self.get_queryset(), many=True)
-    return self.get_paginated_response(self.paginate_queryset(serializer.data))
+def list(request):
+    serializer = EDMSmodel.objects.all()
+    if serializer:
+        serializer = EDMSSerializer(serializer,many = True)
+        return Response(serializer.data)
+    return Response(status=status.HTTP_404_NOT_FOUND)
 
-    # retrieve action
-def retrieve(self, request, pk):
-    # getting object
-    EDMS = self.get_object()
-    serializer = self.get_serializer(EDMS)
-    # returning data as response
-    return Response(serializer.data)
+
+    
+@api_view(['POST'])
+def EDMScreate(request):
+    # Receive the data from the client side
+    dataReceived = request.data
+    serializer = EDMSSerializer(data=dataReceived)
+
+    # Check if data already exist or not
+    if EDMSmodel.objects.filter(**dataReceived).exists():
+        return Response(status=status.HTTP_403_FORBIDDEN)
+    
+    # Validate the data entered by the user
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+   
+
 
 @api_view(['PUT'])
     #modify action
-def update(self,request,pk):
-    EDMS = self.get_object(pk= pk)
-    serializer = self.get_serializer(EDMS)
-    serializer.is_valid(raise_exception=True)
-    serializer.save() 
-    return Response(serializer.data , status= status.HTTP_204_NO_CONTENT)
+def update(request,pk):
+    EDMS = EDMSmodel.objects.get(pk= pk)
+    serializer = EDMSSerializer(instance=EDMS, data=request.data)
+    if serializer.is_valid():
+       serializer.save() 
+       return Response(serializer.data)
+    else: 
+        return Response(status= status.HTTP_204_NO_CONTENT)
+    
+
+       
+@api_view(['PATCH'])
+def modify(request, pk):
+    EDMS = EDMSmodel.objects.get(pk= pk)
+    serializer = EDMSSerializer(instance=EDMS, data=request.data, partial=True)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
        
 
 @api_view(['DELETE'])
     # destroy action
-def destroy(self, request):
+def destroy(request, pk):
     # getting object
-    EDMS = self.get_object()
+    EDMS = EDMSmodel.objects.get(pk=pk)  
     # student deletion
     EDMS.delete()
     # returning status as response
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response(status=status.HTTP_202_ACCEPTED)
+
 
