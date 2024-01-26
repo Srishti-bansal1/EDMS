89ffa7470f03a2b30e2e49e00e30b17452ce0ecc
srishti <srishtibansal241@gmail.com>
2024-01-26 20:45:45 +0530

    add display class in admin

--- EDMSpro/EDMSapp/admin.py
+++ EDMSpro/EDMSapp/admin.py
@@ -9,7 +9,7 @@ from .models import EDMSmodel
 class EDMSReorderFields(admin.ModelAdmin):
     fields = ['name', 'roll_no','email','manager']    # list of fields with the order of fields that are required to be displayed
     search_fields = ['roll_no'] #use for search bar
-    list_display = ('name', 'email','manager') #use to show number not only object 1,obj2 etc
+    list_display = ('name', 'email','id') #use to show number not only object 1,obj2 etc
     list_filter = ("roll_no", )
     
 # registering the Question model
