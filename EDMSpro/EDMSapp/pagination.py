b1cf1b5ab31d9d35ee00fd8d12057cfd8acd8a1c
srishti <srishtibansal241@gmail.com>
2024-01-27 14:54:21 +0530

    add pagination file

---
+++ EDMSpro/EDMSapp/pagination.py
@@ -0,0 +1,22 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class MyPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 4
+    
+    
+    '''def get_page_number(self, request, paginator):
+        """
+        Get the page number from the request's query parameters.
+        """
+        print(paginator)
+        page_number = request.query_params.get(self.page_query_param, 1)
+        try:
+            return int(page_number)
+        except ValueError:
+            return'''
+    
+
+
